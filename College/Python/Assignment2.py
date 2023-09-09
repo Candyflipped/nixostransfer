938d07fd4a901a2b075feae61b0cf4e87841e054
Candyflipped <144550250+Candyflipped@users.noreply.github.com>
2023-09-09 11:30:33 -0500

    Add files via upload

---
+++ College/Python/Assignment2.py
@@ -0,0 +1,23 @@
+# John Kennedy
+# Assignment 2
+# 09/02/2023
+
+# Request the user's name
+your_name = input("What is your name? ")
+
+# Request the user's address, city, state, and zip code
+your_location = input("What is your address, city, state, and zipcode? ")
+
+# Request the user's telephone number
+your_phonenumber = input("What is your phone number? ")
+
+# Request the user's college major
+your_major = input("What is your college major? ")
+
+#Display Information
+print("\n=====Your Information=====")
+print("Your name is: "+your_name+".")
+print("Your address is: "+your_location+".")
+print("Your telephone number is: "+your_phonenumber)
+print("Your college major is: "+your_major+".")
+
